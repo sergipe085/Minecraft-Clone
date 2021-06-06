b7adf55b2e2927adb312de9b9538f1aae2e5e3da
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-06-06 11:24:58 -0300

    building world

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class World : MonoBehaviour {
     public Material textureAtlas = null;
-    public static int chunckSize   = 8;
-    public static int columnHeight = 2;
+
+    public static int chunckSize   = 16;
+    public static int columnHeight = 16;
+    public static int worldSize    = 2;
+
     public static Dictionary<string, Chunck> chuncks;
 
     private void Start() {
         chuncks = new Dictionary<string, Chunck>();
         transform.position = Vector3.zero;
         transform.rotation = Quaternion.identity;
-        StartCoroutine(BuildChunckColumn());
+        StartCoroutine(BuildWorld());
     }
 
     private IEnumerator BuildChunckColumn() {
@@ -22,6 +25,29 @@ public class World : MonoBehaviour {
             Chunck c = new Chunck(chunckPos, textureAtlas);
             c.chunck.transform.parent = this.transform;
             chuncks.Add(c.chunck.name, c);
+        }
+
+        foreach (KeyValuePair<string, Chunck> c in chuncks) {
+            c.Value.DrawChunck();
+            yield return null;
+        }
+
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1)) {
+            
+        }
+    }
+
+    private IEnumerator BuildWorld() {
+        for (int z = 0; z < worldSize; z++) {
+            for (int x = 0; x < worldSize; x++) {
+                for (int i = 0; i < columnHeight; i++) {
+                    Vector3 chunckPos = new Vector3(x * chunckSize, i * chunckSize, z * chunckSize);
+
+                    Chunck c = new Chunck(chunckPos, textureAtlas);
+                    c.chunck.transform.parent = this.transform;
+                    chuncks.Add(c.chunck.name, c);
+                }
+            }
         }
 
         foreach (KeyValuePair<string, Chunck> c in chuncks) {
