3bfe9dfa67b2741ebd00ac7c92554170b2e7a50c
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-06-11 22:43:30 -0300

    limiting camera y axis

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour {
     public void MouseLook(float xInput, float yInput) {
         xTotal += xInput * Time.fixedDeltaTime * sensitivity;
         yTotal += yInput * Time.fixedDeltaTime * sensitivity;
+        yTotal  = Mathf.Clamp(yTotal, -90, 90);
 
         transform.rotation = Quaternion.Euler(-yTotal, xTotal, 0.0f);
     }
